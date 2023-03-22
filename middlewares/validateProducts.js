ba8a8ee95446104bdf8c0665ba9de7be2e05bc55
gemaquejr <gemaquejr@hotmail.com>
2023-03-22 15:05:24 -0300

    Adiciona refatoracao de validateProducts.js

--- middlewares/validateProducts.js
+++ middlewares/validateProducts.js
@@ -1,24 +1,37 @@
-const validateProducts = (req, res, next) => {
-    const { name, quantity } = req.body;
-
-        if (!name) {
-            return res.status(400).json({ message: '"name" is required' });
-        }
-
-        if (name.length < 5) {
-            return res.status(422)
-            .json({ message: '"name" length must be at least 5 characters long' });
-        }
-
-        if (!quantity) {
-            return res.status(400).json({ message: '"quantity" is required' });
-        }
-
-        if (quantity < 1) {
-            return res.status(422)
-            .json({ message: '"quantity" must be greater than or equal to 1' });
-        }
-        return next();
-};
-
-module.exports = validateProducts;
+const validNameUndefined = (req, _res, next) => {
+    const { name } = req.body;
+    if (name === undefined) return next({ statusCode: 400, message: '"name" is required' });
+    next();
+ };
+ 
+ const validNameLength = (req, _res, next) => {
+   const { name } = req.body;
+   if (name.length < 5) {
+     return next({ statusCode: 422, 
+     message: '"name" length must be at least 5 characters long' });
+   }
+   next();
+ };
+ 
+ const validQuantityUndefined = (req, _res, next) => {
+     const { quantity } = req.body;
+     if (quantity === undefined) return next({ statusCode: 400, message: '"quantity" is required' });
+     next();
+  };
+ 
+  const validQuantityLength = (req, _res, next) => {
+    const { quantity } = req.body;
+    if (quantity <= 0) {
+      return next({ statusCode: 422, 
+      message: '"quantity" must be greater than or equal to 1' });
+    }
+    return next();
+  };
+ 
+ module.exports = {
+     validNameUndefined,
+     validNameLength,
+     validQuantityUndefined,
+     validQuantityLength,
+ 
+ };
