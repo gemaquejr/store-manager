8816be3638d997df1fd747a59e722454e18c27f8
gemaquejr <gemaquejr@hotmail.com>
2023-03-22 15:37:50 -0300

    Adiciona productsServices.js

--- services/productsServices.js
+++ services/productsServices.js
@@ -1,54 +1,71 @@
 const productsModels = require('../models/productModel');
 
-const allProducts = async () => {
-    const products = await productsModels.allProducts();
-
-    return products;
+const objErrorNotFound = {
+  error: 404,
+  message: 'Product not found',
+};
+const objErrorExists = {
+  error: 409,
+  message: 'Product already exists',
+};
+const getProductsServices = async () => {
+  const products = await productsModels.getProducts();
+  return products;
 };
 
-const findProductsById = async (id) => {
-    const products = await productsModels.findProductsById(id);
-    if (products.length === 0) {
-        return false;
-    }
-
-    return products[0];
+const getProductsByIdServices = async (id) => {
+  const productsID = await productsModels.getProductsById(id);
+  if (productsID.length < 1) throw objErrorNotFound;
+  return productsID;
 };
 
-const insertProduct = async (name, quantity) => {
-  const sameName = await productsModels.sameNameProduct(name);
-  if (sameName.length !== 0) {
-    return false;
+const validCreate = async (name, quantity) => {
+  const products = await productsModels.getProducts();
+  const validproduct = products.some((e) => e.name === name);
+  if (!validproduct) {
+     const newproduct = await productsModels.createProducts(name, quantity);
+     return newproduct;
   }
-
-  const insertedProduct = await productsModels.insertProduct(name, quantity);
-  return insertedProduct;
+  throw objErrorExists;
 };
 
-const updateProductsById = async (id, name, quantity) => {
-  const products = await productsModels.findProductsById(id);
-  if (products.length === 0) {
-      return false;
+const validUpdate = async (id, name, quantity) => {
+  const products = await productsModels.getProductsById(id);
+  if (products.length > 0) {
+      await productsModels.updateProducts(id, name, quantity);
+      const [productsnew] = await productsModels.getProductsById(id);
+      return productsnew;
   }
-
-  const updatedProduct = await productsModels.updateProductsById({ id, name, quantity });
-  return updatedProduct;
+  throw objErrorNotFound;
 };
 
-const deleteProductsById = async (id) => {
-  const products = await productsModels.findProductsById(id);
-  if (products.length === 0) {
-      return false;
+const validDelete = async (id) => {
+  const products = await productsModels.getProductsById(id);
+  if (products.length > 0) {
+      await productsModels.deleteProducts(id);
+      return true;
   }
+  throw objErrorNotFound;
+};
 
-  const deletedProduct = await productsModels.deleteProductsById(id);
-  return deletedProduct;
+const updateQuantiProducts = async (data) => {
+  await data.forEach(async (e) => {
+      await productsModels.updateProductsQuantityCreate(e.productId, e.quantity);
+  });
+};
+
+const updateQuantiProductsDelete = async (data) => {
+  await data.forEach(async (e) => {
+      await productsModels.updateProductsQuantityDelete(e.productId, e.quantity);
+  });
 };
 
 module.exports = {
-  allProducts,
-  findProductsById,
-  insertProduct,
-  updateProductsById,
-  deleteProductsById,
+  getProductsServices,
+  getProductsByIdServices,
+  validCreate,
+  validUpdate,
+  validDelete,
+  updateQuantiProducts,
+  updateQuantiProductsDelete,
 };
